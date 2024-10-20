f4930edda6661dc260f6a3b080c34f3952d68087
Diego Soto <diegosotomar12@gmail.com>
2024-10-20 11:53:00 -0600

    part 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+using namespace std;
+
+void showBoard(char board [3][3]);
+bool checkWinner(char board[3][3], char player);
 
 int main(){
-    std::cout << "Hello world" << std::endl;
-    return 0;
+    char board[3][3] = {}
+   
+}
+
+void showBoard(char board[3][3])
+{
+    cout << " 0 1 2 " << endl;
+    for (int i = 0;  i< 3; i++)
+    {
+        cout << i << " ";
+        for (int j = 0; j < 3; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool checkWinner(char board[3][3], char player)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+        return true;
+
+        
+    }
 }
